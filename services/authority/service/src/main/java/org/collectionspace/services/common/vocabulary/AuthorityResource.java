90c81f460430df6780ab583645229097a2a6d82e
Richard Millet <richard.millet@berkeley.edu>
2022-01-08 21:10:31 -0800

    CSC-1865: Adding check for urn wildcard.

--- services/authority/service/src/main/java/org/collectionspace/services/common/vocabulary/AuthorityResource.java
+++ services/authority/service/src/main/java/org/collectionspace/services/common/vocabulary/AuthorityResource.java
@@ -1740,4 +1740,21 @@ public abstract class AuthorityResource<AuthCommon, AuthItemHandler>
         return csid;
     }
 
+	public static AuthorityResource<?, ?> getResourceForItem(ResourceMap resourceMap, String tenantId, String itemDocType) {
+		for (String serviceName : resourceMap.keySet()) {
+			CollectionSpaceResource<?, ?> resource = (CollectionSpaceResource<?, ?>) resourceMap.get(serviceName);
+
+			if (resource instanceof AuthorityResource) {
+				AuthorityResource<?, ?> authorityResource = (AuthorityResource<?, ?>) resource;
+				String docType = authorityResource.getItemDocType(tenantId);
+
+				if (docType.equals(itemDocType)) {
+					return authorityResource;
+				}
+			}
+		}
+
+		return null;
+	}
+
 }
